5efc06ddead26c4e6f74d802fa3d5f0c287e49f5
sachaniyanarvin <sachaniyanarvin21@gmail.com>
2025-04-19 03:51:43 +0530

    add app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { GoogleAnalyticsService } from './service/google-analytics.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    HomeComponent,
+    SidebarComponent,
+    FooterComponent,
+    HeaderComponent,
+    // RouterOutlet,
+    // RouterLink,
+    // RouterLinkActive
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DarkTheme';
+export class AppComponent implements OnInit {
+  private lastScrollPosition = 0;
+  isHeaderVisible = true;
+
+  constructor(private googleAnalyticsService: GoogleAnalyticsService) { }
+
+  ngOnInit() {
+    this.googleAnalyticsService.loadGoogleAnalytics();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScroll = window.pageYOffset;
+    this.isHeaderVisible = currentScroll < this.lastScrollPosition || currentScroll < 50;
+    this.lastScrollPosition = currentScroll;
+  }
 }
